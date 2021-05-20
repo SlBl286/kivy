090a7e0bb044a591b425b642c6db0d9a57aa91a7
quy doan duy <quy.doanduy@gmail.com>
2021-05-20 17:30:21 +0700

    a

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from kivy.app import App
+from kivy.properties import StringProperty,BooleanProperty
 from kivy.uix.widget import Widget;
 from kivy.uix.button import Button;
 from kivy.uix.boxlayout import BoxLayout;
+from kivy.uix.gridlayout import GridLayout;
 
-class BoxLayoutExample(BoxLayout):
-    pass;
-    # def __init__(self, **kwargs):
-    #     super().__init__(**kwargs)
-    #     b1 = Button(text='B1');
-    #     b2 = Button(text='B1');
-    #     self.add_widget(b1);
-    #     self.add_widget(b2);
+class LayoutExample(GridLayout):
+    # pass;
+    isEnable = BooleanProperty(False);
+    text = StringProperty("0");
+    def on_button_click(self):
+        if(self.isEnable):
+            self.text = str(int(self.text)+1);
+    def on_toggle_button_click(self,widget):
+        if(widget.state == "normal"):
+            widget.text = "OFF";
+            self.isEnable = False;
+        else:
+            widget.text = "ON"
+            self.isEnable = True;
 
 class MainWidget(Widget):
     pass;
