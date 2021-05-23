4467b1ee8985204e348c557a9d2cd320f6befb5f
duy quy <quy.doanduy@gmail.com>
2021-05-23 14:20:20 +0700

    init screenmanagement

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ from kivy.uix.widget import Widget;
 from kivy.uix.button import Button;
 from kivy.uix.boxlayout import BoxLayout;
 from kivy.uix.gridlayout import GridLayout;
+from kivy.uix.screenmanager import ScreenManager,Screen
 
-class LayoutExample(GridLayout):
-    # pass;
+class LayoutScreen(Screen):
     isEnable = BooleanProperty(False);
+    isSliderDisable = BooleanProperty(True);
     text = StringProperty("0");
     def on_button_click(self):
         if(self.isEnable):
@@ -19,12 +20,21 @@ class LayoutExample(GridLayout):
         else:
             widget.text = "ON"
             self.isEnable = True;
+    def on_switch_active(self,widget):
+        if(widget.active == "OFF"):
+            self.isSliderDisable = True
+        else:
+            self.isSliderDisable = False
 
-class MainWidget(Widget):
+class MenuScreen(Screen):
     pass;
 
 class TheLabApp(App):
-    pass;
+    def build(self):
+        screenManager = ScreenManager();
+        screenManager.add_widget(MenuScreen(name='menu'));
+        screenManager.add_widget(LayoutScreen(name='LayoutScreen'));
+        return screenManager;
 
-
-TheLabApp().run();
+if __name__ == '__main__':
+    TheLabApp().run();
